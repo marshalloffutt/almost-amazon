0a53c71cd313ed6e0eb43a2074576a073be401aa
Marshall Offutt <marshalloffutt@gmail.com>
2018-09-29 14:58:43 -0500

    Working on the store

---
+++ javascripts/components/store.js
@@ -0,0 +1,8 @@
+const book = [
+    {
+        title: "One Hundred Years of Solitude",
+        author: "Gabriel García Márquez",
+        img: "https://upload.wikimedia.org/wikipedia/en/thumb/a/a0/Cien_a%C3%B1os_de_soledad_%28book_cover%2C_1967%29.jpg/220px-Cien_a%C3%B1os_de_soledad_%28book_cover%2C_1967%29.jpg",
+        price: 10.00
+    }
+];
