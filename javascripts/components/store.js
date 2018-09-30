5e54ec8847b015da490c3f1b6514fb5e373effb1
Marshall Offutt <marshalloffutt@gmail.com>
2018-09-30 10:55:43 -0500

    Adds bookCardBuilder on store.js

--- javascripts/components/store.js
+++ javascripts/components/store.js
@@ -6,3 +6,19 @@ const book = [
         price: 10.00
     }
 ];
+
+const createBookCard = () => {
+    let domString = '';
+    domString += ``;
+    for(i = 0; i < book.length; i++) {
+        domString += `<div class="card" style="width: 18rem;">`;
+        domString +=    `<img class="card-img-top" src="${book[i].img}" alt="Cover art for One Hundred Years of Solitude">`
+        domString +=    `<div class="card-body">`;
+        domString +=        `<h5 class="card-title">${book[i].title}</h5>"/>`;
+        domString +=        `<p class="card-text">Almost Amazon Price: ${book[i].price}</p>`;
+        domString +=        `<button type="submit" class="btn btn-primary addToCart">Add to Cart</a>`;
+        domString +=    `</div>`;
+        domString += `</div>`;
+      }
+      printToDom(domString, 'book-card');
+    }
